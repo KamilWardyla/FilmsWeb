5ffc15d4b4ca6418d102ec17edd514bebe1d4533
KamilWardyla <conax13@gmail.com>
2022-08-31 20:34:57 +0200

    django udemy day4

--- filmyweb/views.py
+++ filmyweb/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Film
 
 
-def test_response(request):
-    return HttpResponse("<h1>Kamil</h1>")
+def wszystkie_filmy(request):
+    wszystkie = Film.objects.all() #nazwa_modelu.objects.<ORM>
+    return render(request, "filmy.html", {"filmy": wszystkie})
+# return HttpResponse("<h1>Kamil</h1>")
